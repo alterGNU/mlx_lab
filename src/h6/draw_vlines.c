88004d340cffdaf1edaa4596292fa4de08dc0cc7
alterGNU <alterGNU@gmail.com>
2026-01-20 00:37:36 +0100

    FIX: h6->optimize draw_vlines_little_end_32

--- src/h6/draw_vlines.c
+++ src/h6/draw_vlines.c
@@ -6,7 +6,7 @@
 /*   By: lagrondi <lagrondi.student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 12:13:33 by lagrondi          #+#    #+#             */
-/*   Updated: 2026/01/20 00:00:14 by lagrondi         ###   ########.fr       */
+/*   Updated: 2026/01/20 00:35:12 by lagrondi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	draw_vlines_generic(t_img *img, int x, t_ipos y_inter, int color)
 }
 
 /**
- * NOTE: *(int *)pixel = color could replace the 4 lines setting each byte.
+ * NOTE: *(int *)pixel = color faster than the 4 setting line
  * OPTI: Specialized version for little-endian images with bpp=32 only.
  */
 void	draw_vlines_little_end_32(t_img *img, int x, t_ipos y_inter, int color)
@@ -58,10 +58,7 @@ void	draw_vlines_little_end_32(t_img *img, int x, t_ipos y_inter, int color)
 	pixel = img->addr + (y_inter.x * img->size_line + x * 4);
 	while (y_inter.x <= y_inter.y)
 	{
-		pixel[0] = (color >> 0) & 0xFF;
-		pixel[1] = (color >> 8) & 0xFF;
-		pixel[2] = (color >> 16) & 0xFF;
-		pixel[3] = (color >> 24) & 0xFF;
+		*(int *)pixel = color;
 		pixel += img->size_line;
 		y_inter.x++;
 	}
